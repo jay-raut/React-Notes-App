38fbacd1c1582220549e1617dc2158939b9d843f
jay-raut <jayraut@my.yorku.ca>
2024-06-10 00:03:16 -0400

    Fixed styling and character count in note UI
    Word count when adding a new note now works and text will wrap properly in a new note.

--- front_end/Notes-app/src/components/AddNote.js
+++ front_end/Notes-app/src/components/AddNote.js
@@ -3,20 +3,23 @@ import SaveRoundedIcon from "@mui/icons-material/SaveRounded";
 import { useState } from "react";
 import React from "react";
 import { useContext } from "react";
-
+const characterLimit = 200;
 const AddNote = ({ handleAddNote }) => {
   const [text, setText] = useState("");
   const handleChange = (event) => {
-    setText(event.target.value);
+    if (characterLimit - event.target.value.length >= 0) {
+      setText(event.target.value);
+    }
   };
   const saveClicked = () => {
     handleAddNote(text);
+    setText("");
   };
   return (
     <div className="note new">
-      <textarea rows="8" cols="10" placeholder="Type to create a new note..." className="new-note-textarea" onChange={handleChange}></textarea>
+      <textarea rows="8" cols="10" placeholder="Type to create a new note..." className="new-note-textarea" value={text} onChange={handleChange}></textarea>
       <div className="new-note-footer">
-        <small>{200 - text.length} characters remaining</small>
+        <small>{characterLimit - text.length} characters remaining</small>
         <IconButton aria-label="save" color="inherit" sx={{ zIndex: 1000 }} onClick={() => saveClicked()}>
           <SaveRoundedIcon />
         </IconButton>
